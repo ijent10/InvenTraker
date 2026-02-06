c5d3b7f52dcc37fe67430b7f95983e66d6ba25bc
Ian Jent <ianjjent@icloud.com>
2026-02-05 19:10:16 -0500

    Initial Commit

---
+++ InstaTracker/InstaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaTrackerApp.swift
+//  InstaTracker
+//
+//  Created by Ian Jent on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct InstaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
